3cebdd14f93c9bb6c758b086aa3743e8f44df2ea
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2021-11-21 15:51:09 +0300

    - Nest.js articles test

---
+++ src/article/articles.ts
@@ -0,0 +1,6 @@
+import {Article} from "./article.model";
+
+export const articles = [
+    new Article("Первый пост", "Содержимое первого поста", 1),
+    new Article("Второй пост", "Содержимое второго поста", 2),
+]
